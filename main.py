1f842a537895a3b7231c8930e8d2efaf820f009c
osman abate <abateo@uw.edu>
2017-07-20 14:53:18 -0700

    Added Features to Heroku

--- main.py
+++ main.py
@@ -5,10 +5,11 @@
 # collaborated with "Abdulkarim Abate"
 from flask import Flask, request, redirect, render_template, session
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:lc101@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'mysql+pymysql://blogz:lc101@localhost:8889/blogz')
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key = 'welloethio'
